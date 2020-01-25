f03f61b6a12299ed08db330ef2d5c7815441625b
Chan Hui-Ya <chanhui-ya@ChandeMacBook-Pro.local>
2020-01-25 22:22:02 +0100

    final version

---
+++ guessnumber.py
@@ -0,0 +1,17 @@
+import random 
+r = random.randint(1,100)
+count = 0
+
+while True:
+	count = count + 1
+	num = input ('請輸入數字： ')
+	num = int(num)
+	if num == r:
+		print ('你答對了！')
+		print('你已經猜了', count, '次')
+		break
+	elif num >= r:
+		print ('比答案大')
+	elif num <= r:
+		print ('比答案小')
+	print('你已經猜了', count, '次')
